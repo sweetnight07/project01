b9c7814f878a7786e5e0ae4cffd46f420b13d3fa
John <john_zou@brown.edu>
2023-09-17 16:43:29 -0400

    D

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,31 +1,33 @@
-import * as React from 'react';
-import { TextareaAutosize as BaseTextareaAutosize } from '@mui/base/TextareaAutosize';
-import { styled } from '@mui/system';
+import * as React from "react";
+import { TextareaAutosize as BaseTextareaAutosize } from "@mui/base/TextareaAutosize";
+import { styled } from "@mui/system";
 
 export default function TextArea() {
-  return <TextareaAutosize aria-label="empty textarea" placeholder="Search For Image" />;
+  return (
+    <TextareaAutosize aria-label="empty textarea" placeholder="Description" />
+  );
 }
 
 const blue = {
-  100: '#DAECFF',
-  200: '#b6daff',
-  400: '#3399FF',
-  500: '#007FFF',
-  600: '#0072E5',
-  900: '#003A75',
+  100: "#DAECFF",
+  200: "#b6daff",
+  400: "#3399FF",
+  500: "#007FFF",
+  600: "#0072E5",
+  900: "#003A75",
 };
 
 const grey = {
-  50: '#f6f8fa',
-  100: '#eaeef2',
-  200: '#d0d7de',
-  300: '#afb8c1',
-  400: '#8c959f',
-  500: '#6e7781',
-  600: '#57606a',
-  700: '#424a53',
-  800: '#32383f',
-  900: '#24292f',
+  50: "#f6f8fa",
+  100: "#eaeef2",
+  200: "#d0d7de",
+  300: "#afb8c1",
+  400: "#8c959f",
+  500: "#6e7781",
+  600: "#57606a",
+  700: "#424a53",
+  800: "#32383f",
+  900: "#24292f",
 };
 
 const TextareaAutosize = styled(BaseTextareaAutosize)(
@@ -37,10 +39,12 @@ const TextareaAutosize = styled(BaseTextareaAutosize)(
   line-height: 1.5;
   padding: 12px;
   border-radius: 12px 12px 0 12px;
-  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
-  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
-  border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-  box-shadow: 0px 2px 24px ${theme.palette.mode === 'dark' ? blue[900] : blue[100]};
+  color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+  background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
+  border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
+  box-shadow: 0px 2px 24px ${
+    theme.palette.mode === "dark" ? blue[900] : blue[100]
+  };
 
   &:hover {
     border-color: ${blue[400]};
@@ -48,12 +52,14 @@ const TextareaAutosize = styled(BaseTextareaAutosize)(
 
   &:focus {
     border-color: ${blue[400]};
-    box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[600] : blue[200]};
+    box-shadow: 0 0 0 3px ${
+      theme.palette.mode === "dark" ? blue[600] : blue[200]
+    };
   }
 
   // firefox
   &:focus-visible {
     outline: 0;
   }
-`,
+`
 );
